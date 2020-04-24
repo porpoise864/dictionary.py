9000c6abf8e9fb29df5613fb3986601694506b57
porpoise864 <oertlys@oregonstate.edu>
2020-04-24 15:46:26 -0400

    Initial commit

---
+++ make_employee_dict.py
@@ -0,0 +1,14 @@
+# Author: Sarah Oertly
+# Date: 11/19/2019
+# Description: Write a class named Employee that has data members for an employee's name, ID_number, salary, and
+# email_address (you must use those names - don't make them private). Write a function named make_employee_dict that
+# takes as parameters a list of names, a list of ID numbers, a list of salaries and a list of email addresses. The
+# function should take the first value of each list and use them to create an Employee object. It should do the same
+# with the second value of each list, etc. to the end of the lists. As it creates these objects, it should add them to a
+# dictionary, where the key is the ID number and the value for that key is the whole Employee object. The function
+# should return the resulting dictionary.
+class Person:
+        make_employee_dict = {1:name[], 2: ID[], 3: salary[], 4: email[]}
+        for key in make_employee_dict.items():
+            key = 1,2,3,4
+            return key
